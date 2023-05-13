db29ee68e485a00f816d8afa37bcac39aebb7434
vitorxbranco <vitorxbranco@gmail.com>
2023-05-13 13:35:48 -0300

    added AppBottomBar and news tab

--- app/src/main/java/com/comunidadedevspace/taskbeats/presentation/TaskListActivity.kt
+++ app/src/main/java/com/comunidadedevspace/taskbeats/presentation/TaskListActivity.kt
@@ -18,7 +18,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import java.io.Serializable
 
-class MainActivity : AppCompatActivity() {
+class TaskListActivity : AppCompatActivity() {
 
     private lateinit var ctnContent: LinearLayout
 
@@ -79,7 +79,7 @@ class MainActivity : AppCompatActivity() {
             adapter.submitList(listTasks)
         }
         // LiveData
-        viewModel.taskListLiveData.observe(this@MainActivity, listObserver)
+        viewModel.taskListLiveData.observe(this@TaskListActivity, listObserver)
     }
 
     private fun showMessage(view: View, message: String) {
