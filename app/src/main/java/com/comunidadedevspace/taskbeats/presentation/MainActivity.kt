2cb33caaf87429af7db4af04d763727da4101aa8
vitorxbranco <vitorxbranco@gmail.com>
2023-05-13 16:02:46 -0300

    created fragments and setup clickListener

--- app/src/main/java/com/comunidadedevspace/taskbeats/presentation/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/taskbeats/presentation/MainActivity.kt
@@ -2,11 +2,39 @@ package com.comunidadedevspace.taskbeats.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
 import com.comunidadedevspace.taskbeats.R
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+
+        val taskListFragment = TaskListFragment.newInstance()
+        val newsListFragment = NewsListFragment.newInstance()
+
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container_view, taskListFragment)
+            setReorderingAllowed(true)
+        }
+
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            val fragment = when(menuItem.itemId) {
+                R.id.task_list_item -> taskListFragment
+                R.id.news_list_item -> newsListFragment
+                else -> taskListFragment
+            }
+
+            supportFragmentManager.commit {
+                replace(R.id.fragment_container_view, fragment)
+                setReorderingAllowed(true)
+            }
+
+            true
+        }
     }
 }
